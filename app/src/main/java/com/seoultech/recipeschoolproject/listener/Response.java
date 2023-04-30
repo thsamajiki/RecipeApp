984e9dc020ebd36f0d86d189cbc763e42611449a
thsamajiki <ant1102@naver.com>
2023-05-01 00:21:54 +0900

    Response 클래스에 대한 설명을 주석으로 추가

--- app/src/main/java/com/seoultech/recipeschoolproject/listener/Response.java
+++ app/src/main/java/com/seoultech/recipeschoolproject/listener/Response.java
@@ -1,5 +1,8 @@
 package com.seoultech.recipeschoolproject.listener;
 
+// 응답 객체의 데이터는 제네릭 타입(T)으로 정의되어 있으므로, 어떤 종류의 데이터든 담을 수 있습니다.
+// 이를 통해 서버에서 클라이언트로 다양한 데이터 타입의 응답 객체를 보내어
+// 클라이언트에서 데이터를 적절하게 처리할 수 있습니다.
 public class Response<T> {
     private Type type;
     private T data;
